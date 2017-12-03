4bd666be27d22e1104a098001becf351303ac17a
Scott <sstrubberg@us.ibm.com>
2017-12-02 21:18:33 -0600

    ajax works

--- assets/javascript/gify.js
+++ assets/javascript/gify.js
@@ -1,7 +1,19 @@
+// Array of Star Wars Things
 var topics = ["X-Wing", "Tie-fighter", "B-Wing", "A-wing", "Tie-defender", "Millenium Falcon"];
 
+//Make the buttons on the page from the topics variable.
 $(document).ready(function() {
-    for (var i=0; i<topics.length; i++) {
-        $("#sw-buttons").append("<button type='button' class='btn btn-primary' value=' " + topics[i] + "'>" + topics[i] + "</button>");
+    for (var i = 0;  i < topics.length; i++) {
+        $("#sw-buttons").append("<button type='button' onclick='searchGIFY(\"" + topics[i] + "\")' class='btn btn-primary' value=' " + topics[i] + "'>" + topics[i] + "</button>");
     }
 });
+
+function searchGIFY(starWarsThingName) {
+    $.ajax({
+        url: "https://api.giphy.com/v1/gifs/search?q= " + starWarsThingName + " &api_key=WoKz7UsHBnDyewFCHSNpZPMWvjPsBzZ0&limit=10",
+        method: "GET",
+    })
+    .done(function(response) {
+        console.log(response);
+    })
+}
